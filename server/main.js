4e9258cdb671d954042dfed3eb3232fc07449a95
Umbra-PC09 <ui.juseche@gmail.com>
2019-08-28 18:07:17 -0500

    tailing port

--- server/main.js
+++ server/main.js
@@ -20,7 +20,7 @@ var fs = require('fs');
 var portToListen = 8080
 var server = app.listen({
   host: '127.0.0.1',
-  port: 80,
+  port: process.env.PORT || 3000,
 }, function(data) {
   console.log('------CALLBACKS DATA: ' , data)
   var host = server.address().address;
