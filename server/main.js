4bff4987b7634970e2f2dfb3cadadc9e09f0d29b
Umbra-PC09 <ui.juseche@gmail.com>
2019-08-28 09:06:49 -0500

    tailing port

--- server/main.js
+++ server/main.js
@@ -20,7 +20,7 @@ var io = require('socket.io')(app);
 var fs = require('fs');
 var portToListen = 8080
 var server = app.listen({
-  host: 'hwmonitool.herokuapp.com/',
+  host: 'localhost',
   port: 8080,
 }, function(data) {
   console.log('------CALLBACKS DATA: ' , data)
