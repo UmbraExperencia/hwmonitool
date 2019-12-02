53ada1f37ffb70b6e20ed910d6115b2903793270
Umbra-PC09 <ui.juseche@gmail.com>
2019-12-02 13:47:48 -0500

    k

--- imports/ui/DispositivoCard.js
+++ imports/ui/DispositivoCard.js
@@ -65,7 +65,7 @@ export default class DispositivoCard extends Component {
 
   infoCPUTemperature(id,pDireccionMAC,conectado, estadoEncendido){
     if(conectado){
-      var info = estadoEncendido.replace(/[^\x00-\x7F]/g,"°")
+      var info = estadoEncendido.replace(/[^\x00-\x7F]/g,"°").trim();
 
       this.dialog.show({
         title: 'Detalles de la temperatura',
